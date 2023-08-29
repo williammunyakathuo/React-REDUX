8ad1f0bf39ed40f864955c1091500988e9f678bf
williammunyakathuo <tjwellieyam@gmail.com>
2023-08-29 23:28:17 +0300

    project set up

---
+++ src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IcecreamView = () => {
+  return (
+    <div>
+        <h2>Number of icecream</h2>
+        <button>Order icecream</button>
+        <button>Restock icecream</button>
+    </div>
+  )
+}
